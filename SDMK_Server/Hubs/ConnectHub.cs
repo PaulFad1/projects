4df675ef247a5e28a480444499bdc1d78a4cb6ab
Pavel <paul_fad@icloud.com>
2022-08-26 23:42:24 +0300

    fix: Исправить баг с отображением кзиков
    Поменял вывод информации о кзиках на странице

--- SDMK_Server/Hubs/ConnectHub.cs
+++ SDMK_Server/Hubs/ConnectHub.cs
@@ -3,10 +3,6 @@ namespace BlazorServer.Hubs
 {
     public class ConnectHub: Hub
     {
-        public async Task SendMessage(string n ,  string i, bool r)
-        {
-            await Clients.All.SendAsync("Get",n,i,r);
-        }
     }
 
 }
--- SDMK_Server/Services/ConnectionService.cs
+++ SDMK_Server/Services/ConnectionService.cs
@@ -1,38 +1,39 @@
 using Microsoft.AspNetCore.SignalR.Client;
 using BlazorServer.Hubs;
 using BlazorServer.Data;
+using Microsoft.AspNetCore.SignalR;
+
 public class ConnectionService: BackgroundService 
 {
-    private ConnectHub connectHub;
+    private IHubContext<ConnectHub> connectHub;
     private List<Kzik_Info> kziks;
+
+    public ConnectionService(IHubContext<ConnectHub> hub)
+    {
+        connectHub = hub;
+    }
     protected override async Task ExecuteAsync(CancellationToken token)
     {
-        
+         kziks = new List<Kzik_Info>(){
+            new Kzik_Info("Kzik 1", "192.168.1.1", false),
+            new Kzik_Info("Kzik 2", "192.168.1.2", false),
+            new Kzik_Info("Kzik 3", "192.168.1.3", false)
+        };
         while(!token.IsCancellationRequested)
         {
             try{
-                    foreach(var kz in kziks)
-                    {
-                        await getMessage(kz.Name,kz.IP,kz.isRunning);
-                    }
             }
             catch(Exception e)
             {}
             await Task.Delay(1000);
         }
     }
-    public override async Task StartAsync(CancellationToken token)
+    public async Task getMessage()
     {
-          kziks = new List<Kzik_Info>(){
-            new Kzik_Info("Kzik 1", "192.168.1.1", false),
-            new Kzik_Info("Kzik 2", "192.168.1.2", false),
-            new Kzik_Info("Kzik 3", "192.168.1.3", false)
-        };
-        connectHub = new ConnectHub();
-        await this.ExecuteAsync(token);
-    }
-    private async Task getMessage(string n, string i, bool r)
-    {
-        await connectHub.SendMessage(n,i,r);
+        foreach(var kz in kziks)
+        {
+            await connectHub.Clients.All.SendAsync("Get",kz.Name,kz.IP,kz.isRunning);
+        }
+        
     }
 }
